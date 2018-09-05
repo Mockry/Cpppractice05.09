ffc444d5c6cd2bf34666041aadccc42bd63b10d6
Mockry <43002085+Mockry@users.noreply.github.com>
2018-09-05 14:38:39 +0100

    Added system pause command

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
+// Include libraries
+#include <stdlib.h> // standard library
+
 // entry point for program
 // every c++ program needs a main() function
 int main()
 {
 
 
+
+
+
+	// function that makes the program wait untill a button is pressed to continue
+	system("PAUSE");
 
 	// Tell the operating system we are done
 	// and there was no error
